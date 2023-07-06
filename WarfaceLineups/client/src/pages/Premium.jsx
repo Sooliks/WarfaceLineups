923ccbd29563aa2f72e00b418680068c555c113a
Sooliks <evgenij.terexov.2000@mail.ru>
2023-07-07 01:01:50 +0300

    ready2

--- WarfaceLineups/client/src/pages/Premium.jsx
+++ WarfaceLineups/client/src/pages/Premium.jsx
@@ -1,8 +1,8 @@
 import React, {useContext, useEffect} from 'react';
 
-import {Button} from "antd";
+import {Button, Card, Space, Typography} from "antd";
 import {Context} from "../index";
-
+const { Text } = Typography;
 
 
 
@@ -11,13 +11,34 @@ const Premium = () => {
     const {user} = useContext(Context);
     return (
         <div>
-            <form method="POST" action="https://yoomoney.ru/quickpay/confirm">
-                <input type="hidden" name="receiver" value="4100116427707678"/>
-                <input type="hidden" name="label" value={user.user.login}/>
-                <input type="hidden" name="quickpay-form" value="button"/>
-                <input type="hidden" name="sum" value="350" datatype="number"/>
-                <Button htmlType="submit">Купить Premium</Button>
-            </form>
+            <Card style={{marginTop:20}}>
+                <Space direction={"vertical"}>
+                    <h1>Преимущество премиум аккаунта</h1>
+                    <Card style={{width:500, marginBottom:20}}>
+                        <h2>Без рекламы</h2>
+                        <Text type="secondary">Наслаждайтесь сайтом без ограничений</Text>
+                    </Card>
+                    <Card style={{width:500, marginBottom:20}}>
+                        <h2>Ограничения сняты</h2>
+                        <Text type="secondary">Получайте доступ ко всем материалам и функциям сайта</Text>
+                    </Card>
+                    <Card style={{width:500, marginBottom:20}}>
+                        <h2>Эксклюзивный контент</h2>
+                        <Text type="secondary">Получите доступ к уникальному контенту для премиум пользователей</Text>
+                    </Card>
+                    <Card style={{width:500, marginBottom:20}}>
+                        <h2>Поддержите сайт</h2>
+                        <Text type="secondary">Благодаря двум покупкам Premium вы можете продлить существование сайта на месяц</Text>
+                    </Card>
+                    <form method="POST" action="https://yoomoney.ru/quickpay/confirm">
+                        <input type="hidden" name="receiver" value="4100116427707678"/>
+                        <input type="hidden" name="label" value={user.user.login}/>
+                        <input type="hidden" name="quickpay-form" value="button"/>
+                        <input type="hidden" name="sum" value="350" datatype="number"/>
+                        <Button htmlType="submit" shape="round" size={"large"} style={{width:500}}>Купить Premium</Button>
+                    </form>
+                </Space>
+            </Card>
         </div>
     );
 };
